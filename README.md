068582380e70872d3c3c2cc403c9fbd526f100bf
Spockuto <venkythesupersaiyan@gmail.com>
2015-12-30 00:11:02 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+cli-sms
+=======
+Send SMS from the Command Line
+
+Pushbullet
+==========
+*	This app uses Pushbullet API.
+*	For this script to work , enable Pushbullet on both **Desktop** and **Mobile**.
+*	Get Pushbullet [here](https://www.pushbullet.com/apps)
+
+Usage
+=====
+```sh
+$	python cli-sms.py [API_KEY]	[MOBILE_NUMBER] [MESSAGE]
+```
+
+Get your API-KEY [here](https://www.pushbullet.com/#settings/account)
+
---
+++ cli-sms.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import requests
+import json
+import sys
+
+headers = {'Access-Token' : sys.argv[1]}
+
+
+print "Getting User Identifier..."
+response = requests.get("https://api.pushbullet.com/v2/users/me", headers = headers)
+source_user_iden = str(json.loads(response.text)['iden'])
+
+print "Getting Mobile Identifier..."
+response = requests.get("https://api.pushbullet.com/v2/devices", headers = headers)
+response = json.loads(response.text)
+for i , item in enumerate(response['devices']):
+	print i , item['nickname']
+
+count = input("Select a Mobile: ")
+target_device_iden = str(response['devices'][count]['iden'])
+
+print "Sending Sms"
+final_data = {}
+data = {}
+data.update({"conversation_iden": "+91"+sys.argv[2]})
+data.update({"message": sys.argv[3]})
+data.update({"package_name": "com.pushbullet.android"})
+data.update({"source_user_iden": source_user_iden})
+data.update({ "target_device_iden": target_device_iden})
+data.update({"type": "messaging_extension_reply"})
+final_data.update({"push":data})
+final_data.update({"type":"push"})
+
+headers.update({'Content-Type': 'application/json'})
+response = requests.post("https://api.pushbullet.com/v2/ephemerals", data = json.dumps(final_data) , headers = headers)
+
+if response.text :
+	print "Message Successfully Sent"
